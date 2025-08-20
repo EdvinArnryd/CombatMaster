d54a5e3a9887c03d00897c69f42ded9679e1476d
Edvin <edvin.arn@hotmail.com>
2025-08-20 22:19:17 +0200

    initial

---
+++ CombatMaster/Assets/Scripts/Control/MovementController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // [SerializeField] private char _upKey;
+    // [SerializeField] private char _downKey;
+    // [SerializeField] private char _leftKey;
+    // [SerializeField] private char _rightKey;
+    [SerializeField] private float moveSpeed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= Vector3.left * Time.deltaTime * moveSpeed;
+        }
+    }
+}
