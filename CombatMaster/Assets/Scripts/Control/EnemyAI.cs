eb44c45623b4fe70aa942fd2a3f3d21c0e6d921b
Edvin <edvin.arn@hotmail.com>
2025-08-28 22:18:48 +0200

    Enemy movement

--- CombatMaster/Assets/Scripts/Control/EnemyAI.cs
+++ CombatMaster/Assets/Scripts/Control/EnemyAI.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public Transform checkPoint1;
+    public Transform checkPoint2;
+
+    private bool checkPoint = false;
+
+    public float speed = 0.4f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,21 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (checkPoint)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, checkPoint1.position, Time.deltaTime * speed);
+            if (Vector3.Distance(transform.position, checkPoint1.position) < 1)
+            {
+                checkPoint = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, checkPoint2.position, Time.deltaTime * speed);
+            if (Vector3.Distance(transform.position, checkPoint2.position) < 1)
+            {
+                checkPoint = true;
+            }
+        }
     }
 }
